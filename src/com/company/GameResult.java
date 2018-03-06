de8ed8f775ff44ad1ab344f62bafb392bd6c0c04
jurijs <jurijsmaskaluns@gmail.com>
2018-03-06 20:16:09 +0200

    java 5

---
+++ src/com/company/GameResult.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class GameResult {
+    String userName;
+    int attempts;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,48 +1,62 @@
 package com.company;
 
 
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
 
     public static void main(String[] args) {
+        ArrayList<GameResult> leaderbord = new ArrayList<>(); // hranilishe vsego
         try {
             String answer;
             do {
-                Random random = new Random();
-
-                System.out.println("I think of number from 1 to 100. Try to guess it.");
-                int myNum = random.nextInt(100) + 1;
-                System.out.println(myNum);
-                int i;
-                for (i = 1; i <= 10; i++) {
-                    System.out.print("попытка номер: " + i);
-                    System.out.println(", Введи число: ");
-                    int userNum = askNumber();
-                    if (userNum > myNum) {
-                        System.out.println("Твоё число больше");
-                    } else if (userNum == myNum) {
-                        System.out.println("Бинго !");
-                        break;
-                    } else {
-                        System.out.println("Твоё число меньше");
-                    }
-                }
-                if (i == 11) {
-                    System.out.println("You lost");
+                System.out.print("Enter your name: ");
+                String name = scanner.next();
+                GameResult r = doGame(name);
+                if (r != null) {
+                    leaderbord.add(r);
+//                    System.out.print(r.userName +" ");
+//                    System.out.println(r.attempts);
                 }
                 System.out.println("Once more?");
                 answer = askAnswer();
             } while (answer.equalsIgnoreCase("yes"));
-
-            System.out.println("good bye 1");
+//            System.out.println("good bye 1");
         } catch (NoSuchElementException e1) {
             System.out.println("good bye 2");
         }
+        for (GameResult r : leaderbord)
+        {
+            System.out.println(r.userName + "\t" + r.attempts);
+        }
+        System.out.println("good bye");
+    }
+
+    private static GameResult doGame(String userName) {
+        System.out.println("I think of number from 1 to 100. Try to guess it " + userName);
+        GameResult result = new GameResult(); // sozdajotsja peremennaja drugogo klassa
+        result.userName = userName;
+        int myNum = random.nextInt(100) + 1;
+        System.out.println(myNum);
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("попытка номер: " + i);
+            System.out.println(", Введи число: ");
+            int userNum = askNumber();
+            if (userNum > myNum) {
+                System.out.println("Твоё число больше");
+            } else if (userNum == myNum) {
+                System.out.println("Бинго !");
+                result.attempts = i;
+                return result;
+            } else {
+                System.out.println("Твоё число меньше");
+            }
+        }
+        System.out.println("проиграл ! число было " + myNum);
+        return null;
     }
 
     static String askAnswer() {
